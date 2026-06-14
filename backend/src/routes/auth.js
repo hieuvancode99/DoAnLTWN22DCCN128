747416576206a9dba26a8f94be9be9faf3d08d24
Van Hieu <nvanhieu007@gmail.com>
2026-06-14 15:49:10 +0700

    refactor: remove OTP registration feature completely

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser, sendOtp } = require('../controllers/authController');
+const { registerUser, loginUser } = require('../controllers/authController');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.post('/send-otp', sendOtp);
 
 module.exports = router;
